3a3d410c89699fb1f1af832e7ef61a41e09c2bf1
Rimanagi <rim.linking@mail.ru>
2023-04-07 17:33:16 +0300

    Blur-Bid v1.0

--- main.py
+++ main.py
@@ -1,32 +1,38 @@
 # pip install -r requirements
 from openpyxl import load_workbook
 from openpyxl.styles import PatternFill
+import linecache
+
 
 # input here .txt filename
-input_txt_file = open(input("Enter .txt filename: ") + ".txt", 'r')
-print(input_txt_file)
+filename = input("Enter .txt filename: ") + ".txt"
+input_txt_file = open(filename, 'r')
 # input here .xlsx filename
 output_xlsx_file = load_workbook(input("Enter .xlsx filename: ") + ".xlsx")
+ws = output_xlsx_file.active
 
-if __name__ == '__main__':
 
-    blur_bid_word = "Blur Bid: "
-    ws = output_xlsx_file.active
-    row, count = 2, 3
-    value_to_compare = float(input("Минимальное желаемое значение Blur Bid: "))
+row = 2
+line_number = 0
+blur_bid_word = "Blur Bid Profit: "
+value_to_compare = float(input("Минимальное желаемое значение Blur Bid: "))
 
-    for line in input_txt_file:
-        if count % 4 == 0 and blur_bid_word in line:
-            line = line.split()
-            blur_bid_value = float(line[-1])
+
+for line in input_txt_file:
+    line_number += 1
+
+    if blur_bid_word in line:
+        blur_bid_profit = float(line.split()[-1])
+
+        if float(blur_bid_profit) >= value_to_compare:
+            line = linecache.getline(filename, line_number - 1)
+            blur_bid_value = float(line.split()[-1])
             ws.cell(row=row, column=5).value = blur_bid_value
-            # value to change                  ↓
-            if float(blur_bid_value) > value_to_compare:
-                ws.cell(row=row, column=5).fill = PatternFill("solid", fgColor="61f551")
-            else:
-                ws.cell(row=row, column=5).fill = PatternFill("solid", fgColor="FF4567")
-            row += 1
-        count += 1
+            ws.cell(row=row, column=5).fill = PatternFill("solid", fgColor="61f551")
+        else:
+            break
+        row += 1
 
-    input_txt_file.close()
-    output_xlsx_file.save("forOffers.xlsx")
+linecache.clearcache()
+input_txt_file.close()
+output_xlsx_file.save("2.xlsx")
